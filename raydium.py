2d11bec28c23b8479276650ac8987ccb34f6204b
Likio3000 <abethuneplou@gmail.com>
2024-05-31 12:10:29 +0200

    added the node https "safely"

--- raydium.py
+++ raydium.py
@@ -1,3 +1,22 @@
 import pandas as pd
 import numpy as np
+import solana
+import plotly as plt
+from dotenv import load_dotenv, find_dotenv
+import os
 
+# Find and load the .env file
+dotenv_path = find_dotenv("settings.env")
+if dotenv_path:
+    load_dotenv(dotenv_path)
+    print("Loaded .env file from:", dotenv_path)
+else:
+    print("Could not find .env file")
+
+# Access the .env variables
+endPoint = os.getenv("endPoint")
+
+if endPoint:
+    print(f'endPoint: {endPoint}')
+else:
+    print("endPoint variable not found")
